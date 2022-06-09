d0a34ea8f85d062e38f2697eadbba127a128ceec
tolehoai <tolehoai@gmail.com>
2022-06-09 10:53:54 +0700

    feat/lucky-number

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+    public function number(): Response{
+        $number = random_int(0,100);
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
