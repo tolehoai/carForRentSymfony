1b8a1100fa9458e386069b52ced7f0343f6c93aa
tolehoai <tolehoai@gmail.com>
2022-06-14 11:14:24 +0700

    feat/add-api-authentication

---
+++ src/Controller/API/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\API;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/api/admin', name: 'app_api_admin')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message'=>'Admin homepage'
+        ]);
+    }
+}
---
+++ src/Controller/API/ProfileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\API;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/api/profile', name: 'app_api_profile')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message'=>'Profile homepage'
+        ]);
+    }
+}
