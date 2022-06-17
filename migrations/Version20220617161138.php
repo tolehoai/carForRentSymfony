fae26390d605d8722c3aaf860968407a0361508f
tolehoai <tolehoai@gmail.com>
2022-06-17 23:57:35 +0700

    fix/fix-env-in-local-laptop

--- migrations/Version20220617161138.php
+++ migrations/Version20220617161138.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220617041336 extends AbstractMigration
+final class Version20220617161138 extends AbstractMigration
 {
     public function getDescription(): string
     {
