26961c05af94f0e9752926e85ce75138d5ebfbad
tolehoai <tolehoai@gmail.com>
2022-06-22 13:56:32 +0700

    feat/add-image-validator

--- src/Controller/API/ImageController.php
+++ src/Controller/API/ImageController.php
@@ -6,6 +6,7 @@ use App\Request\ImageUploadRequest;
 use App\Service\ImageService;
 use App\Traits\ResponseTrait;
 use App\Traits\TransferTrait;
+use App\Validator\ImageValidator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,11 +18,14 @@ class ImageController extends AbstractController
     use TransferTrait;
 
     #[Route('/api/image', name: 'app_api_image', methods: 'POST')]
-    public function index(Request $request, ImageUploadRequest $imageUploadRequest, ImageService $imageService): JsonResponse
+    public function index(Request $request, ImageValidator $imageValidator, ImageUploadRequest $imageUploadRequest, ImageService $imageService): JsonResponse
     {
         $file = $request->files->get('image');
         $imageUploadRequest->setImage($file);
-
+        $errors = $imageValidator->validatorImageRequest($imageUploadRequest);
+        if (!empty($errors)) {
+            return $this->error($errors);
+        }
         $image = $this->objectToArray($imageService->upload($file));
 
         return $this->success($image);
--- src/Request/ImageUploadRequest.php
+++ src/Request/ImageUploadRequest.php
@@ -8,10 +8,13 @@ use Symfony\Component\Validator\Constraints as Assert;
 class ImageUploadRequest extends BaseRequest
 {
     #[Assert\Image(
-        minWidth: 200,
-        maxWidth: 400,
-        minHeight: 200,
-        maxHeight: 400,
+        maxSize: '3M',
+        mimeTypes: [
+            'image/jpeg',
+            'image/jpg',
+            'image/png'
+        ],
+        mimeTypesMessage: 'The type of the file is invalid ({{ type }}). Allowed types are {{ types }}.'
     )]
     private File $image;
 
